4db88379abe251166bb16afcba232e6547de197a
abbk2234 <Kahosseini90@gmail.com>
2025-02-05 21:27:16 +0330

    update version

---
+++ script/modules/Slider.js
@@ -0,0 +1,97 @@
+const homeSliderBanner = new Swiper("#home-slider-banner", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    pauseOnMouseEnter: true,
+  },
+  slidesPerView: 1,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+const homeSliderSale = new Swiper("#home-slider-sale", {
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    pauseOnMouseEnter: true,
+  },
+  slidesPerView: 4,
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    576: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 6,
+    },
+  },
+});
+const blogCate = new Swiper("#blog-cate", {
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    pauseOnMouseEnter: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".btn-swiper-next",
+    prevEl: ".btn-swiper-prev",
+  },
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 3,
+    },
+    350: {
+      slidesPerView: 4,
+    },
+    576: {
+      slidesPerView: 5,
+    },
+    768: {
+      slidesPerView: 6,
+    },
+    992: {
+      slidesPerView: 8,
+    },
+  },
+});
+// const serviceSlider = new Swiper(".service-slider", {
+//   loop: true,
+//   autoplay: true,
+//   speed: 400,
+//   centeredSlides: true,
+//   spaceBetween: 50,
+//   breakpoints: {
+//     0: {
+//       slidesPerView: 1,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     992: {
+//       slidesPerView: 3,
+//     },
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+export {homeSliderBanner, homeSliderSale, blogCate};
